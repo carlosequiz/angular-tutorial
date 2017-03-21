c679de8f05e732b617a91dc4c83e39b670800b1c
Carlos Equiz <carlosequiz@gmail.com>
2017-03-21 09:28:18 -0400

    Declarando componente en modulo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { HeaderComponent } from './shared/layout/header.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+ imports: [ 
+ BrowserModule
+ ],
+ declarations: [
+ AppComponent,
+ HeaderComponent
+ ],
+ bootstrap: [
+ AppComponent
+ ]
 })
 export class AppModule { }
